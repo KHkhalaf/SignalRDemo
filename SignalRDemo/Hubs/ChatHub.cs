6ed192abd0434e149c5af644c226e7262ef4b873
hp <hp@DESKTOP-FKTABFC>
2019-09-17 22:21:55 +0300

    this a commit

---
+++ SignalRDemo/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRDemo.Models
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string fromUser,string Message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", fromUser, Message);
+        }
+    }
+}
---
+++ SignalRDemo/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", function (fromUser, Message){
+    var msg = fromUser + " : " + Message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").append(li);
+});
+
+connection.start();
+
+$("#btnSend").on("click", function () {
+    var fromUser = $("#txtUser").val();
+    var message = $("#txtMessage").val();
+
+    connection.invoke("SendMessage", fromUser, message);
+});
